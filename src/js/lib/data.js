316432f8f45a772089eec6b5bb7b34d2329311ee
Shreyas Minocha <shreyasminocha7@gmail.com>
2018-09-13 16:25:36 +0530

    Add some more networks

--- src/js/lib/data.js
+++ src/js/lib/data.js
@@ -23,7 +23,9 @@ const accessPoints = [
         ssid: 'Who wants free WiFi',
         centre: new Point(45, 3),
         radius: 24,
-        speed: { download: 1, upload: 1 }
+        speed: { download: 1, upload: 1 },
+        password: 2208706,
+        encryption: 'WEP'
     }, {
         ssid: 'Free WiFi Initiative',
         centre: new Point(18, -24),
@@ -33,13 +35,13 @@ const accessPoints = [
         ssid: 'Prada WiFi',
         centre: new Point(-16, 0),
         radius: 40,
-        speed: { download: 10, upload: 20 },
+        speed: { download: 10, upload: 10 },
         cost: 240
     }, {
         ssid: 'AndroidAP',
         centre: new Point(29, -15),
         radius: 15,
-        speed: { download: 10, upload: 20 }
+        speed: { download: 7.5, upload: 800 / 1024 }
     }, {
         ssid: 'Linksys',
         centre: new Point(-5, 7),
@@ -49,7 +51,7 @@ const accessPoints = [
         ssid: 'PuzzleConf',
         centre: new Point(8, 24),
         radius: 24,
-        speed: { download: 10, upload: 20 }
+        speed: { download: 8, upload: 15 }
     }, {
         ssid: 'Azure Diamond',
         centre: new Point(14, -10),
@@ -61,7 +63,7 @@ const accessPoints = [
         ssid: 'Hyatt',
         centre: new Point(18, 24),
         radius: 20,
-        speed: { download: 7.5, upload: 10 },
+        speed: { download: 10, upload: 10 },
         cost: 300
     }, {
         ssid: 'xfinitywifi',
@@ -72,18 +74,51 @@ const accessPoints = [
         ssid: 'xfinitywifi',
         centre: new Point(40, -5),
         radius: 40,
-        speed: { download: 10, upload: 20 },
+        speed: { download: 5, upload: 15 },
         cost: 160
     }, {
         ssid: 'Muller home',
         centre: new Point(-7, -7),
         radius: 33,
-        speed: { download: 10, upload: 20 }
+        speed: { download: 4, upload: 8 }
+    },
+    {
+        ssid: 'Airtel',
+        centre: new Point(-80, 50),
+        radius: 80,
+        speed: { download: 20, upload: 20 }
+    },
+    {
+        ssid: 'Junk',
+        centre: new Point(-17, 18),
+        radius: 60,
+        speed: { download: 2, upload: 14 },
+        cost: 50
+    },
+    {
+        ssid: 'SystemAdmin',
+        centre: new Point(20, 20),
+        radius: 30,
+        speed: { download: 5, upload: 6 }
+    },
+    {
+        ssid: '🐌',
+        centre: new Point(0, 0),
+        radius: 100,
+        speed: { download: 256 / 1024, upload: 256 / 1024 }
     },
     {
         ssid: '⏩',
         centre: new Point(57.1, 57.1),
         radius: 0.5,
         speed: { download: 100, upload: 100 }
+    },
+    {
+        ssid: 'shreyas',
+        centre: new Point(75, 40),
+        radius: 40,
+        speed: { download: 20, upload: 20 },
+        password: 1064706847,
+        encryption: 'WPA2 Personal'
     }
 ].map(options => new AccessPoint(options));
